d653595dd8aed33a8b23670946d18c0c9aa0ff62
asj <105547098+zeus1560@users.noreply.github.com>
2025-05-21 10:25:44 +0900

    Add files via upload

---
+++ 안승주 _0514.py
@@ -0,0 +1,125 @@
+import os
+import dotenv
+import streamlit as st
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
+
+
+dotenv.load_dotenv()
+
+
+@st.cache_resource
+def load_and_split_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    return loader.load_and_split()
+
+def create_vector_store(_docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    split_docs = text_splitter.split_documents(_docs)
+    embedding = OpenAIEmbeddings(model='text-embedding-3-small')
+    vectorstore = FAISS.from_documents(split_docs, embedding)
+    FAISS.save_local(vectorstore, "./faiss_db")
+    return vectorstore
+
+@st.cache_resource
+def get_vectorstore(_docs):
+    persist_directory = "./faiss_db"
+    if os.path.exists(os.path.join(persist_directory, "index.faiss")):
+        embedding = OpenAIEmbeddings(model='text-embedding-3-small')
+        return FAISS.load_local(
+            persist_directory,
+            embedding,
+            allow_dangerous_deserialization=True 
+        )
+    else:
+        return create_vector_store(_docs)
+
+
+
+@st.cache_resource
+def initialize_components(selected_model):
+    file_path = r"C:\RAG\data\헌법.pdf"  
+    pages = load_and_split_pdf(file_path)
+    vectorstore = get_vectorstore(pages)
+    retriever = vectorstore.as_retriever()
+
+    
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Keep the answer perfect. please use imogi with the answer.
+    대답은 한국어로 하고, 존댓말을 써줘.\
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    llm = ChatOpenAI(model=selected_model)
+    history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    return rag_chain
+
+
+st.header("헌법 Q&A 챗봇 💬 📚")
+option = st.selectbox("Select GPT Model", ("gpt-4o-mini", "gpt-3.5-turbo-0125"))
+rag_chain = initialize_components(option)
+chat_history = StreamlitChatMessageHistory(key="chat_messages")
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: chat_history,
+    input_messages_key="input",
+    history_messages_key="history",
+    output_messages_key="answer",
+)
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", 
+                                     "content": "헌법에 대해 무엇이든 물어보세요!"}]
+
+for msg in chat_history.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+
+if prompt_message := st.chat_input("Your question"):
+    st.chat_message("human").write(prompt_message)
+    with st.chat_message("ai"):
+        with st.spinner("Thinking..."):
+            config = {"configurable": {"session_id": "any"}}
+            response = conversational_rag_chain.invoke(
+                {"input": prompt_message},
+                config)
+            
+            answer = response['answer']
+            st.write(answer)
+            with st.expander("참고 문서 확인"):
+                for doc in response['context']:
+                    st.markdown(doc.metadata['source'], help=doc.page_content)
